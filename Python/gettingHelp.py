df29f2b4798c2e55586f746eb2129eabf4a57dd6
grassnhi <nhi.nguyenn@hcmut.edu.vn>
2024-02-04 15:53:33 +0700

    Another course.

--- Python/gettingHelp.py
+++
@@ -1,6 +0,0 @@
-help(round)
-
-# Python evaluates an expression like this from the inside out. 
-# First it calculates the value of round(-2.01), then it provides help on the output of that expression.
-help(round(-2.01))
-
